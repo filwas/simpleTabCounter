4bc06dd57a6d923f5097b90add9e8e8b35122317
Filip Was <fwas.corp@gmail.com>
2023-04-12 20:53:52 +0200

    moreListeners

--- background.js
+++ background.js
@@ -7,6 +7,7 @@ async function tabUpdate() {
 tabUpdate();
 chrome.tabs.onCreated.addListener(() => tabUpdate());
 chrome.tabs.onRemoved.addListener(() => tabUpdate());
+chrome.tabs.onActivated.addListener(() => tabUpdate());
 chrome.windows.onCreated.addListener(() => tabUpdate());
 chrome.windows.onRemoved.addListener(() => tabUpdate());
 chrome.windows.onFocusChanged.addListener(() => tabUpdate());
