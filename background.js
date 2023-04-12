10f1f37b1b771ab9a1a435c4c1c87336ffe4898a
Filip Was <fwas.corp@gmail.com>
2023-04-12 20:26:31 +0200

    firstUpload

---
+++ background.js
@@ -0,0 +1,12 @@
+async function tabUpdate() {
+    chrome.tabs.query({currentWindow: true}).then(
+        (fulfill) => chrome.action.setBadgeText({text: fulfill.length.toString()})
+    )
+}
+
+tabUpdate();
+chrome.tabs.onCreated.addListener(() => tabUpdate());
+chrome.tabs.onRemoved.addListener(() => tabUpdate());
+chrome.windows.onCreated.addListener(() => tabUpdate());
+chrome.windows.onRemoved.addListener(() => tabUpdate());
+
