2db309f0b1bd5794316d8af34a4e2a30f250426f
Filip Wąs <69075808+filwas@users.noreply.github.com>
2023-06-12 15:46:06 +0200

    doesn't update input when you paste into input

--- contentScript.js
+++ contentScript.js
@@ -2,8 +2,10 @@ document.addEventListener('paste', (event)=>{
     let amount = event.clipboardData.getData('text/plain').match(/\d+\.\d{2}$/)[0];
     let lastQuerableElement = document.querySelector("#main-content > div:nth-child(6) > div > div:nth-child(1) > div");
     let inputElement = lastQuerableElement.childNodes[3].firstChild.firstChild.querySelector("input[name='received']");
-    inputElement.value = amount
-    inputElement.dispatchEvent(new InputEvent("input", {bubbles: true}));
+    if (event.target != inputElement) {
+      inputElement.value = amount
+      inputElement.dispatchEvent(new InputEvent("input", {bubbles: true}));
+    }
   })
   
   
